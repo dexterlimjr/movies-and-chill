898f95a6e8a3fbf281ebaa74fdabcd574d70adce
Jia Rong <jr.loves.maths@gmail.com>
2018-12-28 00:42:02 +0800

    refactor: use https for images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const API_KEY = process.env.REACT_APP_API_KEY
 export const BASE_URL = 'https://api.themoviedb.org/3'
-export const ASSET_URL = 'http://image.tmdb.org/t/p'
+export const ASSET_URL = 'https://image.tmdb.org/t/p'
